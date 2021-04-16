cbb939b08458cbaa5585c2774f6a44abb4d46798
Tiago Bernardes <tiago@tiagobernardes.com.br>
2021-04-16 15:35:52 -0300

    Create verifica-categoria-checkout.php

---
+++ WooCommerce/verifica-categoria-checkout.php
@@ -0,0 +1,19 @@
+<? //não adicionar essa linha
+
+add_action('woocommerce_checkout_before_customer_details', 'lab82_verifica_categoria_carrinho');
+function lab82_verifica_categoria_carrinho()
+{
+	$cat_in_cart = false;
+	foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
+		if (has_term('CATEGORIA-DE-PRODUTO-AQUI', 'product_cat', $cart_item['product_id'])) {
+			$cat_in_cart = true;
+			break;
+		}
+	}
+	// Do something if the category is in the Cart
+	if ($cat_in_cart) {
+		echo 'CONTEÚDO PERSONALIZADO AQUI';
+	}
+}
+
+//não adicionar essa linha ?>
