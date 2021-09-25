ecafd96b3b35be87db2e9e362a3fbae1b9d2bb2e
Tiago Bernardes <tiago@tiagobernardes.com.br>
2021-09-25 17:42:15 -0300

    criado arquivo remover blocos do widgets

---
+++ remove-widget-bloco.php
@@ -0,0 +1,6 @@
+/*REMOVE WIDGET EM BLOCO*/
+// Desabilita o editor de bloco em widgets do plugin Gutenberg.
+add_filter( 'gutenberg_use_widgets_block_editor', '__return_false', 100 );
+
+// Desabilita o editor de bloco dos widgets. renomeado de wp_use_widgets_block_editor
+add_filter( 'use_widgets_block_editor', '__return_false' );
