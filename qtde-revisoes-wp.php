abf4fc8ae010ff1a52f646e541d6e5981212368c
Tiago Bernardes <tiago@tiagobernardes.com.br>
2021-09-25 17:49:47 -0300

    criado arquivo controle de quantidade de versões

---
+++ qtde-revisoes-wp.php
@@ -0,0 +1,8 @@
+<? //não incluir essa linha
+// Adicionar código no wp-config.php do seu wordpress.
+//a função deste snippet é reduzir a quantidade de versões criadas pelo wordpress
+
+/*número de revisões | substitua o 3 pela quantidade desejada*/
+define( 'WP_POST_REVISIONS', 3 );
+
+//não incluir essa linha ?>
